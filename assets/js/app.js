f70f14897fd52e30abb00712865245abdf6c7fcf
Tegorinternational <104412526+Tegorinternational@users.noreply.github.com>
2022-08-15 17:18:27 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ const data = [
     getappname: "Extra Spaces Remover tool",
     getappdesc: "Tegor International",
     getappversion: "1.2.0",
-    getapplogo: "../imgs/headlogo.png"
+    getapplogo: "https://tegorinternational.github.io/ESCR/assets/imgs/headlogo.png"
   }
   ];
  data;
